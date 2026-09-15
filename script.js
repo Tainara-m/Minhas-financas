13d2d1c1440be8a19968f0e0a7d6a285efad40e9
Tainara-m <tainaramartinscarvalho@gmail.com>
2026-09-15 12:29:49 -0300

    Implementação do salvamento de tema no localStorage

--- script.js
+++ script.js
@@ -18,14 +18,21 @@ function alternarGuia(event, idGuia){
         const html = document.documentElement;
         const btnTema = document.getElementById("theme-toggle");
         const iconeTema = document.getElementById("theme-icon");
+        const temaSalvo = localStorage.getItem("data-theme");
+
+        if(temaSalvo !== null){
+            html.setAttribute("data-theme",temaSalvo);
+        }
 
        btnTema.addEventListener('click', () =>{
         const temaAtual = html.getAttribute("data-theme");
             if(temaAtual === 'light'){
                 html.setAttribute("data-theme","dark");
+                localStorage.setItem("data-theme","dark")
                 iconeTema.textContent = "dark_mode"
             }else{
                 html.setAttribute("data-theme","light");
+                localStorage.setItem("data-theme","light")                
                 iconeTema.textContent = "light_mode"
             }
         }
